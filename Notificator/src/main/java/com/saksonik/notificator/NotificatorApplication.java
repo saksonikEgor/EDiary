9a36ad588b21342213b491eaea789c4d68d706d1
Saksonik Egor <saksonik.egor@mail.ru>
2024-03-17 14:24:32 +0300

    Add Notificator module

---
+++ Notificator/src/main/java/com/saksonik/notificator/NotificatorApplication.java
@@ -0,0 +1,13 @@
+package com.saksonik.notificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificatorApplication.class, args);
+	}
+
+}
