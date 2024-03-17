86dfb42eb9237adad82ed58cf39ef4f2d696dc7f
Saksonik Egor <saksonik.egor@mail.ru>
2024-03-17 14:15:58 +0300

    Initial commit

---
+++ HeadManager/src/main/java/com/saksonik/headmanager/HeadManagerApplication.java
@@ -0,0 +1,13 @@
+package com.saksonik.headmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadManagerApplication.class, args);
+    }
+
+}
---
+++ HeadManager/src/test/java/com/saksonik/headmanager/HeadManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saksonik.headmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
