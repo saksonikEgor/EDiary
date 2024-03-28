51e90cdf0e86814a93420aa615b587c2e41f89d3
saksonikEgor <saksonik.egor@mail.ru>
2024-03-28 21:16:01 +0300

    OAuth2 initial

--- HeadManager/src/main/java/com/saksonik/headmanager/controller/UserController.java
+++ HeadManager/src/main/java/com/saksonik/headmanager/controller/UserController.java
@@ -1,12 +1,19 @@
 package com.saksonik.headmanager.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/test")
 public class UserController {
-    @GetMapping("/hm")
+    @GetMapping("/login")
     public String get() {
         return "hm";
+    }
+
+    @GetMapping("/internal")
+    public String internal(){
+        return "internal work";
     }
 }
