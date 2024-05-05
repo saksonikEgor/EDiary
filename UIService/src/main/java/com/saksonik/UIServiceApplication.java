0931b09508e89f1f47888cbd87b3911b4e0217d3
saksonikEgor <saksonik.egor@mail.ru>
2024-05-05 15:26:22 +0300

    UI service initial

---
+++ UIService/src/main/java/com/saksonik/UIServiceApplication.java
@@ -0,0 +1,11 @@
+package com.saksonik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UIServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UIServiceApplication.class, args);
+    }
+}
