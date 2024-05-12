a2ee3e60465d9052b22c677a805e1a383af5e231
saksonikEgor <saksonik.egor@mail.ru>
2024-05-12 15:17:16 +0300

    Add logout

--- UIService/src/main/java/com/saksonik/configuration/SecurityConfig.java
+++ UIService/src/main/java/com/saksonik/configuration/SecurityConfig.java
@@ -1,16 +1,21 @@
 package com.saksonik.configuration;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
+@Slf4j
 @Configuration
 public class SecurityConfig {
     @Bean
-    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http, ServerLogoutSuccessHandler handler) {
         return http
                 .authorizeExchange(customizer -> customizer
                         .pathMatchers("/img/**", "/js/**", "/css/**")
@@ -19,7 +24,18 @@ public class SecurityConfig {
                         .permitAll()
                         .anyExchange().authenticated())
                 .oauth2Login(Customizer.withDefaults())
+                .logout(logoutSpec -> logoutSpec.logoutSuccessHandler(handler))
                 .oauth2Client(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public ServerLogoutSuccessHandler keycloakLogoutHandler(ReactiveClientRegistrationRepository repository) {
+        OidcClientInitiatedServerLogoutSuccessHandler oidcLogoutSuccessHandler =
+                new OidcClientInitiatedServerLogoutSuccessHandler(repository);
+
+        oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}/");
+
+        return oidcLogoutSuccessHandler;
     }
 }
